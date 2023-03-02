10c63268285a1a01d4c1885bd52d3ed9a11776f9
amradhabalia-unigo <83628656+amradhabalia-unigo@users.noreply.github.com>
2023-03-02 14:59:29 +0100

    Add files via upload

---
+++ Figure1/A_BUSCO/result/Busco_RunChlorokybus_atmophyticus.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH -p medium
+#SBATCH -t 10:00:00
+#SBATCH -o Busco_%J.out
+#SBATCH -C scratch2
+#SBATCH --exclusive
+#SBATCH --mem 100G
+#SBATCH --mail-type=BEGIN,END
+#Delete this line and add your email :)
+#SBATCH --mail-user=amradhabalia.ashok@uni-goettingen.de
+
+#Clear everything to create a clean environment 
+module purge
+#Load busco
+module load busco
+
+#Input = fasta file of interest
+IN=/usr/users/ashok/OrthologsProject/SpeciesDB/OF_run_remove_them_completely_from_this_dir/Chlorokybus_atmophyticus_protein_replaced_ast.fa
+#Mode uncomment the one you want to use
+#MODE=genome
+MODE=proteins
+#MODE=transcriptome
+#Output folder
+OUT=busco_out
+
+#Busco Run
+#Auto lineage with only eukaryotic it will find the best eukaryotic database
+#busco -m $MODE -i $IN -o $OUT -f --auto-lineage-euk
+
+#Busco with specified lineage selection (eukaryota_odb10) In your case the lineage Iker mentioned :)
+busco -m $MODE -i $IN -o $OUT -f -l chlorophyta_odb10 -o Chlorokybus_atmophyticus_BUSCO_out
